b77f5e42249046a99ebe50e3a4d7893b126093a0
N_Magi <csma17068@g.nihon-u.ac.jp>
2020-12-22 00:59:07 +0900

    プロジェクト ファイルを追加します。

---
+++ MonitorPowerOff/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace MonitorPowerOff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MonitorOFF");
+            win32.PostMessage((IntPtr)0xFFFF, 0x0112, 0xF170, 2);
+        }
+    }
+
+    class win32
+    {
+        [DllImport("User32.dll")]
+        public static extern void PostMessage(IntPtr hwind, ushort wMsg, ushort wParam, short lParam);
+    }
+}
